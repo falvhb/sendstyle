c9af46572c79cddcc17a78d34320fd3ae8f28990
Glitch (sendstyle) <none>
2018-04-30 13:52:32 +0000

    👜🍿 Updated with Glitch

--- public/sendstyle.js
+++ public/sendstyle.js
@@ -26,18 +26,25 @@ function loadScript(url, callback){
 var URL = 'https://sendstyle.glitch.me/';
 
 window._sendstyles.forEach(function(style){
-    style.$link = document.querySelector('link[href="'+style[0]+'"]');
-    var newLink = (URL.substr(0,4) === 'http' ? '' : URL) + style[1];
-    style.$link.href = newLink + "?" + new Date().getTime();
+    style.$links = document.querySelectorAll('link[href="'+style[0]+'"]');
+    var newLink = (style[1].substr(0,4) === 'http' ? '' : URL.replace('sendstyle',window._sendstyleId)) + style[1];
+    //console.log(newLink, )  
+    for (var i = 0,ii=style.$links.length;i<ii;i+=1){
+      style.$links[i].href = newLink + "?" + new Date().getTime();
+    }
 });
 
 loadScript(URL + 'socket.io/socket.io.js', function(){
     //initialization code
     var socket = io.connect(URL);
     socket.on('css', function (data) {      
+      var cachebuster = new Date().getTime();
       if (data.id === window._sendstyleId &&  window._sendstyles.length > 0){
         window._sendstyles.forEach(function(style){
-          style.$link.href=style.$link.href.split('?')[0]+"?" + new Date().getTime()
+          for (var i = 0,ii=style.$links.length;i<ii;i+=1){
+            style.$links[i].href=style.$links[i].href.split('?')[0]+"?" + cachebuster;
+          }
+          
         });
       }
     });
